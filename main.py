53f2efecbd4676bde9d07fa4c68e14700b67642f
gabrielfelipeg <gabriel.gomes@ccc.ufcg.edu.br>
2018-10-23 14:36:12 -0300

    adding manager main

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import downloader
+
+def main():
+    
+    sub_regions_raster = []
+    for region in regions:
+        ndvi_calculate = []
+        for scene in region:
+            scene_raster = download( scene )
+            ndvi_calculate.append( ndvi_calculate(scene_raster) )
+        sub_region = merge(region)
+        sub_regions_raster.append( crop(sub_region) )
+    merge( sub_regions_raster )
+
+
+if __name__ == '__main__':
+    main()
