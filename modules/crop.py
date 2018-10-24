e15833e7bf1db5bfea2fc6d9c04242d5b58f05d4
thiagowhispher <thiagoyeds2013@gmail.com>
2018-10-24 16:31:20 -0300

    Adding module crop complete

--- modules/crop.py
+++ modules/crop.py
@@ -1,11 +1,19 @@
 import os
 
-def crop(src_path, shape_path, output_path = 'crop_sub_regions/'):
+def crop(src_path, shape_path, output_name, output_path = 'crop_sub_regions/'):
 
     os.system('mkdir -p ' + output_path)
 
+    output_path_src_cropped = output_path + output_name + '.tif'
+
     separator = ' '
-    comand = separator.join( ('gdalwarp -cutline', shape_path, src_path, '-dstnodata -nan') )
+    command = separator.join( ('gdalwarp -cutline', shape_path, src_path) )
+    command = separator.join( (command, output_path_src_cropped, '-dstnodata -nan') )
 
-    os.system(comand)
+    os.system(command)
 
+    #os.system('rm -r src_path')
+
+    return output_path_src_cropped
+
+crop('sample1/ndvi.tif', 'shapefile/Limite_Externo_AMJJ.shp', 'AMJJ')
