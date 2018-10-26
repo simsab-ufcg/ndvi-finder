1eac5c8a506955c33133861d8895c7c7bb0f867c
Matheus <matheus7op@gmail.com>
2018-10-26 09:54:00 -0300

    Bug fix

--- modules/mergeTool.py
+++ modules/mergeTool.py
@@ -50,8 +50,7 @@ def merge(path_list, output_name="output"):
 	os.system("rm -f " + output_name + str(alt^1))
 	os.system("rm -f aux.tif")
 
-	output_path = output_name + '.tif'
-	return output_path
+	return resulting_tiff_path
 
 
 #path_list = ['ndvi_scenes/ndvi_scene_0.tif', 'ndvi_scenes/ndvi_scene_1.tif', 'ndvi_scenes/ndvi_scene_2.tif']
