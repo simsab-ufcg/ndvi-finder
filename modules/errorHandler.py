d8dd874949cb057289e87ce633a69d232d1b1459
Matheus <matheus7op@gmail.com>
2018-11-01 15:47:21 -0300

    Adding error handler module.

---
+++ modules/errorHandler.py
@@ -0,0 +1,18 @@
+def throwError(step, code):
+	exit_code = (code >> 8)
+	signal_code = (511 & code)
+
+	switch = {
+		1: 'Could not open TIFF file.'
+		2: 'Could not read from TIFF file.'
+		3: 'Unsupported band in TIFF file.'
+		4: 'Could not write in TIFF file.'
+		5: 'Could not read MTL file.'
+	}
+
+	if switch.has_key(exit_code):
+		print 'Error while in ' + step + ' step. ' + switch[exit_code]
+	else:
+		print 'Unexpected error: signal error was ' + str(signal_code) + '. Step: ' + step
+
+	raise SystemExit
