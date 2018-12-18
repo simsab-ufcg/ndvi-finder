42b69e5b39019b435a3cad1bfdc6a218f97f5d33
thiagowhispher <thiagoyeds2013@gmail.com>
2018-12-18 17:12:18 -0300

    Finish crop tests

--- unittest/tests/testCrop.py
+++ unittest/tests/testCrop.py
@@ -1,6 +1,38 @@
+from modules import utils as ut
 
 def set_up():
-    return
+    inputs = {
+        'test01': ['inputs/crop/shapefile/full.shp', 'inputs/crop/any_tiff.tif', 'results/output_any_tiff_full.tif'],
+        'test02': ['inputs/crop/shapefile/empty.shp', 'inputs/crop/any_tiff.tif', 'results/output_any_tiff_empty.tif'],
+        'test03': ['inputs/crop/shapefile/top.shp', 'inputs/crop/any_tiff.tif', 'results/output_any_tiff_top.tif'],
+        'test04': ['inputs/crop/shapefile/down.shp', 'inputs/crop/any_tiff.tif', 'results/output_any_tiff_down.tif'],
+        'test05': ['inputs/crop/shapefile/left.shp', 'inputs/crop/any_tiff.tif', 'results/output_any_tiff_left.tif'],
+        'test06': ['inputs/crop/shapefile/right.shp', 'inputs/crop/any_tiff.tif', 'results/output_any_tiff_right.tif']
+    }
+
+    outputs = {
+        'test01': 'outputs/crop/any_tiff_full.tif',
+        'test02': 'outputs/crop/any_tiff_empty.tif',
+        'test03': 'outputs/crop/any_tiff_top.tif',
+        'test04': 'outputs/crop/any_tiff_down.tif',
+        'test05': 'outputs/crop/any_tiff_left.tif',
+        'test06': 'outputs/crop/any_tiff_right.tif'
+    }
+
+    return inputs, outputs
+
+def test_crop(inputs, outputs, tests):
+    for test in tests:
+        exit_code = ut.run_crop(inputs[test])
+        ut.verify_result(exit_code)
+
+        exit_code = ut.compare_tiffs(outputs[test], inputs[test][2])
+        ut.verify_result(exit_code)
+
+        ut.remove_file(inputs[test][2])
 
 if __name__ == '__main__':
-    set_up()
+    tests = ['test01', 'test02', 'test03', 'test04', 'test05', 'test06']
+
+    inputs, outputs = set_up()
+    test_crop(inputs, outputs, tests)
--- unittest/tests/testNdvi.py
+++ unittest/tests/testNdvi.py
@@ -1,44 +1,43 @@
-from modules import utilsNdvi as un
-from modules import utilsAll as ua
+from modules import utils as ut
 
 def set_up_l8():
     inputs = {
-        'test01': ['inputs/l8_b4.tif', 'inputs/l8_b5.tif', 'inputs/l8_bqa_full.tif', 'inputs/l8_mtl.txt', 'results/output_ndvi_t1.tif'],
-        'test02': ['inputs/l8_b4.tif', 'inputs/l8_b5.tif', 'inputs/l8_bqa_empty.tif', 'inputs/l8_mtl.txt', 'results/output_ndvi_t2.tif'],
-        'test03': ['inputs/l8_b4.tif', 'inputs/l8_b5.tif', 'inputs/l8_bqa_top.tif', 'inputs/l8_mtl.txt', 'results/output_ndvi_t3.tif'],
-        'test04': ['inputs/l8_b4.tif', 'inputs/l8_b5.tif', 'inputs/l8_bqa_down.tif', 'inputs/l8_mtl.txt', 'results/output_ndvi_t4.tif'],
-        'test05': ['inputs/l8_b4.tif', 'inputs/l8_b5.tif', 'inputs/l8_bqa_left.tif', 'inputs/l8_mtl.txt', 'results/output_ndvi_t5.tif'],
-        'test06': ['inputs/l8_b4.tif', 'inputs/l8_b5.tif', 'inputs/l8_bqa_right.tif', 'inputs/l8_mtl.txt', 'results/output_ndvi_t6.tif'],
+        'test01': ['inputs/ndvi/l8_b4.tif', 'inputs/ndvi/l8_b5.tif', 'inputs/ndvi/l8_bqa_full.tif', 'inputs/ndvi/l8_mtl.txt', 'results/output_ndvi_t1.tif'],
+        'test02': ['inputs/ndvi/l8_b4.tif', 'inputs/ndvi/l8_b5.tif', 'inputs/ndvi/l8_bqa_empty.tif', 'inputs/ndvi/l8_mtl.txt', 'results/output_ndvi_t2.tif'],
+        'test03': ['inputs/ndvi/l8_b4.tif', 'inputs/ndvi/l8_b5.tif', 'inputs/ndvi/l8_bqa_top.tif', 'inputs/ndvi/l8_mtl.txt', 'results/output_ndvi_t3.tif'],
+        'test04': ['inputs/ndvi/l8_b4.tif', 'inputs/ndvi/l8_b5.tif', 'inputs/ndvi/l8_bqa_down.tif', 'inputs/ndvi/l8_mtl.txt', 'results/output_ndvi_t4.tif'],
+        'test05': ['inputs/ndvi/l8_b4.tif', 'inputs/ndvi/l8_b5.tif', 'inputs/ndvi/l8_bqa_left.tif', 'inputs/ndvi/l8_mtl.txt', 'results/output_ndvi_t5.tif'],
+        'test06': ['inputs/ndvi/l8_b4.tif', 'inputs/ndvi/l8_b5.tif', 'inputs/ndvi/l8_bqa_right.tif', 'inputs/ndvi/l8_mtl.txt', 'results/output_ndvi_t6.tif'],
     }
 
     outputs = {
-        'test01': 'outputs/l8_ndvi_full.tif',
-        'test02': 'outputs/l8_ndvi_empty.tif',
-        'test03': 'outputs/l8_ndvi_top.tif',
-        'test04': 'outputs/l8_ndvi_down.tif',
-        'test05': 'outputs/l8_ndvi_left.tif',
-        'test06': 'outputs/l8_ndvi_right.tif',
+        'test01': 'outputs/ndvi/l8_ndvi_full.tif',
+        'test02': 'outputs/ndvi/l8_ndvi_empty.tif',
+        'test03': 'outputs/ndvi/l8_ndvi_top.tif',
+        'test04': 'outputs/ndvi/l8_ndvi_down.tif',
+        'test05': 'outputs/ndvi/l8_ndvi_left.tif',
+        'test06': 'outputs/ndvi/l8_ndvi_right.tif',
     }
 
     return inputs, outputs
 
 def set_up_l5():
     inputs = {
-        'test01': ['inputs/l5_b3.tif', 'inputs/l5_b4.tif', 'inputs/l5_bqa_full.tif', 'inputs/l5_mtl.txt', 'results/output_ndvi_t1.tif'],
-        'test02': ['inputs/l5_b3.tif', 'inputs/l5_b4.tif', 'inputs/l5_bqa_empty.tif', 'inputs/l5_mtl.txt', 'results/output_ndvi_t2.tif'],
-        'test03': ['inputs/l5_b3.tif', 'inputs/l5_b4.tif', 'inputs/l5_bqa_top.tif', 'inputs/l5_mtl.txt', 'results/output_ndvi_t3.tif'],
-        'test04': ['inputs/l5_b3.tif', 'inputs/l5_b4.tif', 'inputs/l5_bqa_down.tif', 'inputs/l5_mtl.txt', 'results/output_ndvi_t4.tif'],
-        'test05': ['inputs/l5_b3.tif', 'inputs/l5_b4.tif', 'inputs/l5_bqa_left.tif', 'inputs/l5_mtl.txt', 'results/output_ndvi_t5.tif'],
-        'test06': ['inputs/l5_b3.tif', 'inputs/l5_b4.tif', 'inputs/l5_bqa_right.tif', 'inputs/l5_mtl.txt', 'results/output_ndvi_t6.tif'],
+        'test01': ['inputs/ndvi/l5_b3.tif', 'inputs/ndvi/l5_b4.tif', 'inputs/ndvi/l5_bqa_full.tif', 'inputs/ndvi/l5_mtl.txt', 'results/output_ndvi_t1.tif'],
+        'test02': ['inputs/ndvi/l5_b3.tif', 'inputs/ndvi/l5_b4.tif', 'inputs/ndvi/l5_bqa_empty.tif', 'inputs/ndvi/l5_mtl.txt', 'results/output_ndvi_t2.tif'],
+        'test03': ['inputs/ndvi/l5_b3.tif', 'inputs/ndvi/l5_b4.tif', 'inputs/ndvi/l5_bqa_top.tif', 'inputs/ndvi/l5_mtl.txt', 'results/output_ndvi_t3.tif'],
+        'test04': ['inputs/ndvi/l5_b3.tif', 'inputs/ndvi/l5_b4.tif', 'inputs/ndvi/l5_bqa_down.tif', 'inputs/ndvi/l5_mtl.txt', 'results/output_ndvi_t4.tif'],
+        'test05': ['inputs/ndvi/l5_b3.tif', 'inputs/ndvi/l5_b4.tif', 'inputs/ndvi/l5_bqa_left.tif', 'inputs/ndvi/l5_mtl.txt', 'results/output_ndvi_t5.tif'],
+        'test06': ['inputs/ndvi/l5_b3.tif', 'inputs/ndvi/l5_b4.tif', 'inputs/ndvi/l5_bqa_right.tif', 'inputs/ndvi/l5_mtl.txt', 'results/output_ndvi_t6.tif'],
     }
 
     outputs = {
-        'test01': 'outputs/l5_ndvi_full.tif',
-        'test02': 'outputs/l5_ndvi_empty.tif',
-        'test03': 'outputs/l5_ndvi_top.tif',
-        'test04': 'outputs/l5_ndvi_down.tif',
-        'test05': 'outputs/l5_ndvi_left.tif',
-        'test06': 'outputs/l5_ndvi_right.tif',
+        'test01': 'outputs/ndvi/l5_ndvi_full.tif',
+        'test02': 'outputs/ndvi/l5_ndvi_empty.tif',
+        'test03': 'outputs/ndvi/l5_ndvi_top.tif',
+        'test04': 'outputs/ndvi/l5_ndvi_down.tif',
+        'test05': 'outputs/ndvi/l5_ndvi_left.tif',
+        'test06': 'outputs/ndvi/l5_ndvi_right.tif',
     }
 
     return inputs, outputs
@@ -46,13 +45,13 @@ def set_up_l5():
 
 def test_ndvi(inputs, outputs, tests):
     for test in tests:
-        exit_code = un.run_ndvi(inputs[test])
-        ua.verify_result(exit_code)
+        exit_code = ut.run_ndvi(inputs[test])
+        ut.verify_result(exit_code)
 
-        exit_code = ua.compare_tiffs(outputs[test], inputs[test][4])
-        ua.verify_result(exit_code)
+        exit_code = ut.compare_tiffs(outputs[test], inputs[test][4])
+        ut.verify_result(exit_code)
 
-        ua.remove_file(inputs[test][4])
+        ut.remove_file(inputs[test][4])
 
 
 if __name__ == '__main__':
